3bcc2bde77b90421c703263cbf129b20b0414644
8Leprechaun8 <dmvoyevodin@mail.ru>
2025-10-20 05:33:14 +0300

    Added logging.

--- src/main/java/com/example/bankcards/service/impl/BankcardsServiceImpl.java
+++ src/main/java/com/example/bankcards/service/impl/BankcardsServiceImpl.java
@@ -12,6 +12,9 @@ import com.example.bankcards.repository.UserRepository;
 import com.example.bankcards.service.BankcardsService;
 import com.example.bankcards.specification.CardSpecification;
 import com.example.bankcards.specification.SearchCriteria;
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
@@ -25,8 +28,10 @@ import java.util.UUID;
 import java.util.stream.Collectors;
 
 @Service
+@Slf4j
 public class BankcardsServiceImpl implements BankcardsService {
 
+    private static final Logger log = LoggerFactory.getLogger(BankcardsServiceImpl.class);
     private static double EPS = 0.00001;
 
     @Autowired
@@ -43,6 +48,7 @@ public class BankcardsServiceImpl implements BankcardsService {
         Card card = cardMapper.cardDtoToEntity(cardDto);
         User user = userRepository.getReferenceById(cardDto.getUserId());
         if (user == null) {
+            log.error("Пользователь, которому будет соответствовать карта, не найден");
             throw new UserNotFoundException("Пользователь, которому будет соответствовать карта, не найден");
         }
         card.setUser(user);
@@ -81,6 +87,7 @@ public class BankcardsServiceImpl implements BankcardsService {
     public Double getBalanceForCardByUser(UUID id, String number) throws CardNotFoundException {
         Double balance = cardRepository.getBalanceForCardByUser(id, number);
         if (balance == null) {
+            log.error("Не найдена карта");
             throw new CardNotFoundException("Не найдена карта");
         }
         return balance;
@@ -92,6 +99,7 @@ public class BankcardsServiceImpl implements BankcardsService {
         Card card = cardRepository.findByUserIdAndNumberAndIsArchived(id, number, false);
         if (card != null) {
             if (!card.getStatus().equals(CardStatus.ACTIVE)) {
+                log.error("Статус карты не активен");
                 throw new CardStatusNotActiveException("Статус карты не активен");
             }
             cardRepository.setStatusWaitingForBlocking(id, number);
@@ -109,6 +117,7 @@ public class BankcardsServiceImpl implements BankcardsService {
         validateCardForSendingMoney(cardTo);
         double diff = cardFrom.getBalance() - sum;
         if (cardFrom.getBalance() <= 0 || (diff < 0) && (Math.abs(diff) > EPS)) {
+            log.error("Сумма на карте " + numberFrom + " слишком мала для перевода");
             throw new SendingMoneyIsProhibitedException("Сумма на карте " + numberFrom + " слишком мала для перевода");
         } else if ((diff < 0) && (Math.abs(diff) < EPS)) {
             sum = cardFrom.getBalance();
@@ -120,9 +129,11 @@ public class BankcardsServiceImpl implements BankcardsService {
     private void validateCardForSendingMoney(Card card) throws CardStatusNotActiveException, CardNotFoundException {
         if (card != null) {
             if (!card.getStatus().equals(CardStatus.ACTIVE)) {
+                log.error("Статус карты не активен");
                 throw new CardStatusNotActiveException("Статус карты не активен");
             }
         } else {
+            log.error("Не найдена карта");
             throw new CardNotFoundException("Не найдена карта");
         }
     }
@@ -132,9 +143,11 @@ public class BankcardsServiceImpl implements BankcardsService {
     public void blockCard(UUID id) throws CardStatusNotWaitingForBlockingException, CardNotFoundException {
         Card card = cardRepository.getReferenceById(id);
         if (card.getArchived()) {
+            log.error("Карта не найдена");
             throw new CardNotFoundException("Карта не найдена");
         }
         if (!CardStatus.WAITING_FOR_BLOCKING.equals(card.getStatus())) {
+            log.error("Карта не находится в состоянии ожидания блокирования");
             throw new CardStatusNotWaitingForBlockingException("Карта не находится в состоянии ожидания блокирования");
         }
         card.setStatus(CardStatus.BLOCKED);
@@ -146,9 +159,11 @@ public class BankcardsServiceImpl implements BankcardsService {
     public void activateCard(UUID id) throws CardNotFoundException, CardStatusNotWaitingForBlockingException {
         Card card = cardRepository.getReferenceById(id);
         if (card.getArchived()) {
+            log.error("Карта не найдена");
             throw new CardNotFoundException("Карта не найдена");
         }
         if (!CardStatus.BLOCKED.equals(card.getStatus())) {
+            log.error("Карта не находится в состоянии блокировки");
             throw new CardStatusNotWaitingForBlockingException("Карта не находится в состоянии блокировки");
         }
         card.setStatus(CardStatus.ACTIVE);
@@ -160,6 +175,7 @@ public class BankcardsServiceImpl implements BankcardsService {
     public void deleteCard(UUID id) throws CardNotFoundException {
         Card card = cardRepository.getReferenceById(id);
         if (card.getArchived()) {
+            log.error("Карта не найдена");
             throw new CardNotFoundException("Карта не найдена");
         }
         card.setArchived(true);
